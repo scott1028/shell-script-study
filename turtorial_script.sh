452d3cc0040884b180310f63291be3c2068c9918
scott1028 <mic1028002@gmail.com>
2014-02-09 12:09:53 +0800

    update code

--- turtorial_script.sh
+++ turtorial_script.sh
@@ -1,20 +1,20 @@
 # define a function
 function dosomething()
 {
-	echo after_dosomething
-	expr $1 + 100
+	echo after_dosomething;
+	expr $1 + 100;
 }
 
 # demo if-else statements
 if [ 1 == 1 ]
 then
-	echo 2
-	echo 20
-	dosomething 200
+	echo 2;
+	echo 20;
+	dosomething 200;
 else
-	echo 3
-	echo 30
-	dosomething 300
+	echo 3;
+	echo 30;
+	dosomething 300;
 fi
 
 # test if variables $tt defined
